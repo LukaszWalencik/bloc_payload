ea8f4819632059c1c1dfae13a94cacbbedf77366
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-08-16 23:51:57 +0200

    Update payload bloc, add there if statement to change AppTheme value depense of randInt value

--- lib/blocs/payload/payload_bloc.dart
+++ lib/blocs/payload/payload_bloc.dart
@@ -1,12 +1,17 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_payload/blocs/payload/payload_state.dart';
 import 'package:equatable/equatable.dart';
 
 part 'payload_event.dart';
-part 'payload_state.dart';
 
 class PayloadBloc extends Bloc<PayloadEvent, PayloadState> {
-  PayloadBloc() : super(PayloadInitial()) {
-    on<PayloadEvent>((event, emit) {
+  PayloadBloc() : super(PayloadState.initial()) {
+    on<ChangeThemeEvent>((event, emit) {
+      if (event.randInt % 2 == 0) {
+        emit(state.copyWith(appTheme: AppTheme.light));
+      } else {
+        emit(state.copyWith(appTheme: AppTheme.dark));
+      }
       // TODO: implement event handler
     });
   }
