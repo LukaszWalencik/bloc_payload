e29bf52c7393d6df2f65fc0290bc213b207e0e2b
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-08-16 23:48:14 +0200

    Update payload event, create ChangeThemeEvent there with randInt variable

--- lib/blocs/payload/payload_event.dart
+++ lib/blocs/payload/payload_event.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'payload_bloc.dart';
 
 sealed class PayloadEvent extends Equatable {
@@ -5,4 +6,16 @@ sealed class PayloadEvent extends Equatable {
 
   @override
   List<Object> get props => [];
+}
+
+class ChangeThemeEvent extends PayloadEvent {
+  final int randInt;
+
+  ChangeThemeEvent(this.randInt);
+
+  @override
+  String toString() => 'ChangeThemeEvent(randInt: $randInt)';
+
+  @override
+  List<Object> get props => [randInt];
 }
